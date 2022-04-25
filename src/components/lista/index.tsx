1e74bbcfefd191605a625687b3d1e67dfa1b0a73
Jessica <jessicaferreira200223@gmail.com>
2022-04-25 14:45:14 -0300

    AgendaFinalizada

---
+++ src/components/lista/index.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { ITarefas } from '../../types/Tarefas';
+import Item from './item';
+import style from './Lista.module.scss'
+
+interface Props {
+    tarefas: ITarefas[],
+    selecionaTarefa:(TarefaSelecionada:ITarefas) => void
+}
+
+export function Lista({ tarefas, selecionaTarefa}: Props) {
+       
+    return(
+        <aside className={style.listaTarefas}>
+            <h2>Estudos do Dia</h2>
+            <ul>              
+                {tarefas.map( item =>(
+                    <Item
+                        selecionaTarefa={selecionaTarefa}
+                        key={item.id}
+                        {...item}
+                    />
+                ))}
+            </ul>            
+        </aside>
+    )
+}
+export default Lista
